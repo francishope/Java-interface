d1d638f4c9c8ad005402525db813418e5fd6fea8
francishope <130940812+francishope@users.noreply.github.com>
2024-06-04 07:57:11 +0100

    Create array.java

---
+++ array.java
@@ -0,0 +1,39 @@
+package interfaces;
+
+
+
+public class array {
+
+    public static void main(String[] args) {
+
+        int[] numbersArray = new int[5];
+
+        numbersArray[0] = 17;
+
+        numbersArray[1] = 34;
+
+        numbersArray[2] = 54;
+
+        numbersArray[3] = 77;
+
+        numbersArray[4] = 47;
+
+        
+
+        for (int i = numbersArray.length-1; i >=0; i--){
+
+            System.out.println(numbersArray[i]);
+
+            //if(numbersArray[i] == 54) {
+
+                //System.out.println("Found " + numbersArray[i]);
+
+                //break;
+
+            //}
+
+        }
+
+    }
+
+    }
