2d2f66827bdb586dfe84a4fd3699fc8c6d40b4a5
francishope <130940812+francishope@users.noreply.github.com>
2024-06-04 07:54:33 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class main{
+
+    public static void main(){
+
+        String word = "level";
+
+        char[] wordList = word.toCharArray();
+
+        for (i = wordList.length -1, i >= 0, i--){
+
+            String word2 += wordList[i]; 
+
+        }
+
+        if (word == word2){
+
+            System.out.println("The word is the same backword");
+
+        }
+
+        else{
+
+            System.out.println("The word is not the same backword");
+
+        }
+
+    }
+
+  }
